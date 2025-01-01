f313a980204df0654a43edb0091f119a6300ea33
Giovani Rocha Lima <giovani.espressolabs@mastercctvm.com.br>
2025-01-01 07:47:40 -0300

    Add AddProductCommandValidator for command validation
    Introduce AddProductCommandValidator in Api.Products.v1.AddProduct
    namespace. This class inherits from AbstractValidator<AddProductCommand>
    and uses FluentValidation to enforce rules ensuring that the Name,
    Category, and ImageFile properties are not empty and that the Price
    property is greater than 0.

---
+++ Catalog/src/Api/Products/v1/AddProduct/AddProductCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace Api.Products.v1.AddProduct;
+
+public class AddProductCommandValidator : AbstractValidator<AddProductCommand>
+{
+    public AddProductCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+                .WithMessage("Name is required");
+
+        RuleFor(x => x.Category)
+            .NotEmpty()
+                .WithMessage("Category is required");
+
+        RuleFor(x => x.ImageFile)
+            .NotEmpty()
+                .WithMessage("ImageFile is required");
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0)
+                .WithMessage("Price must be greater than 0");
+    }
+}
